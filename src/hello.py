3c200a4de9aa2b92662f25170f3d169b77909a9b
Alberto Camara <camara@protonmail.com>
2020-12-03 07:06:19 +0100

    Initial commit

---
+++ src/hello.py
@@ -0,0 +1,11 @@
+def sum(x: int, y: int) -> int:
+    return x + y
+
+
+def main():
+    print("Hello world!")
+    print(f"2 + 3 = {sum(2, 3)}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_hello.py
@@ -0,0 +1,4 @@
+import hello
+
+def test_sum():
+    assert hello.sum(2, 3) == 5
