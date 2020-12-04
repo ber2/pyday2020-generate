8634e6d004d185f5a70c88530592e71649caee8b
Alberto Camara <camara@protonmail.com>
2020-12-04 08:49:30 +0100

    Generate timestamps (#14)
    Uniform distribution

--- src/generators.py
+++ src/generators.py
@@ -1,7 +1,27 @@
+import datetime as dt
 import uuid
 
+import numpy as np
 import pandas as pd
 
 
 def generate_voter_ids(length: int) -> pd.Series:
     return pd.Series([str(uuid.uuid1()) for _ in range(length)], dtype="object")
+
+
+def generate_timestamps(length: int) -> pd.Series:
+    if length == 0:
+        return pd.Series(data=None, dtype="datetime64[ns]")
+    date = dt.date(2020, 12, 10)
+    hour_range = range(8, 21)
+
+    hours = [dt.time(h) for h in np.random.choice(hour_range, size=length)]
+
+    data = pd.DataFrame(hours)
+    data["date"] = date
+    data["hour"] = hours
+
+    data["timestamp"] = data.apply(
+        lambda row: pd.Timestamp.combine(row["date"], row["hour"]), axis=1
+    )
+    return data["timestamp"]
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -2,7 +2,9 @@ import pytest
 from hypothesis import given
 import hypothesis.strategies as st
 
-from generators import generate_voter_ids
+import pandas as pd
+
+from generators import generate_voter_ids, generate_timestamps
 
 
 @given(st.integers(min_value=0, max_value=2000))
@@ -19,3 +21,20 @@ def test_generated_ids_have_proper_length(length):
 @given(st.integers(min_value=0, max_value=2000))
 def test_generated_ids_have_no_repetitions(length):
     assert len(generate_voter_ids(length).drop_duplicates()) == length
+
+
+@given(st.integers(min_value=0, max_value=2000))
+def test_generated_as_many_timestamps_as_requested(length):
+    assert generate_timestamps(length).shape == (length,)
+
+
+@given(st.integers(min_value=0, max_value=2000))
+def test_generated_timestamps_all_have_constant_date(length):
+    dates = list(generate_timestamps(length).dt.date.unique())
+    assert not dates or dates == [pd.Timestamp("2020-12-10")]
+
+
+@given(st.integers(min_value=0, max_value=2000))
+def test_generated_timestamps_all_have_hours_within_range(length):
+    hours = generate_timestamps(length).dt.hour.unique()
+    assert all(hour in range(8, 21) for hour in hours)
