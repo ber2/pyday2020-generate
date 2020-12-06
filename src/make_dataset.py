998a88a25c7864e98f924599f5886eee2bb4381a
Alberto Camara <camara@protonmail.com>
2020-12-06 21:00:00 +0100

    WIP This works but is too slow

--- src/make_dataset.py
+++ src/make_dataset.py
@@ -4,27 +4,14 @@ from multiprocessing import Pool
 import pandas as pd
 
 from generators import (
-    generate_voter_ids,
-    generate_timestamps,
-    generate_regions,
-    generate_votes,
+    generate_votes
 )
 
 
-def make_dataframe(length: int) -> pd.DataFrame:
-    result = pd.DataFrame()
-    result["timestamp"] = generate_timestamps(length)
-    result["id"] = generate_voter_ids(length)
-    result["region"] = generate_regions(length)
-    result["vote"] = generate_votes(length)
-
-    return result
-
-
-def generate_one_file(index: int, path_pattern: str) -> None:
-    path = path_pattern + f"_{index}.csv"
+def generate_one_file(index: int) -> None:
+    path = f"../data/votes_{index}.csv"
     tic = time.time()
-    make_dataframe(2000000).to_csv(path, index=False)
+    generate_votes(2000000).to_csv(path, index=False)
     tac = time.time()
     print(f"Generated 2M rows in file: {index}\t{tac - tic} seconds")
 
@@ -32,8 +19,11 @@ def generate_one_file(index: int, path_pattern: str) -> None:
 def main() -> None:
 
     print("Will generate 72 files with 2M rows each")
-    for index in range(72):
-        generate_one_file(index, "../data/votes")
+
+    with Pool() as pl:
+        pl.map(generate_one_file, list(range(72)))
+    # for index in range(72):
+        # generate_one_file(index, "../data/votes")
     print("All done")
 
 
